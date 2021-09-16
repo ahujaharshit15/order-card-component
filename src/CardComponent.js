f0ef49122942061d5203e11e38ca1dd3fe5408fd
Harshit Ahuja <ahujaharshit15@gmail.com>
2021-09-17 00:33:56 +0530

    Making basic outline.

--- src/CardComponent.js
+++ src/CardComponent.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-import "./CardComponent.css"
+import "./CardComponent.css";
 
-const CardComponent = ()=>{
-   return <h1>Works.</h1>
-}
+const CardComponent = () => {
+  return (
+    <React.Fragment>
+      <div class="outer-box">
+        <div >
+            <img class="image-box" alt="Unicorn" src="https://images.unsplash.com/photo-1454372182658-c712e4c5a1db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"></img>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
-export default CardComponent
+export default CardComponent;
