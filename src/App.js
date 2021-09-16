4dca62cd6ce035026c703fa3dff0e04c01b10b60
Harshit Ahuja <ahujaharshit15@gmail.com>
2021-09-16 22:23:50 +0530

    Setting up component.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import CardComponent from "./CardComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <CardComponent />
+    </div>
+  );
+}
+
+export default App;
